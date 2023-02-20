2b580569fd15f308e729583c8c7f02f6615d2002
shahzadnetlink <shahzad@netlinkcontrol.com>
2023-02-21 02:13:54 +0500

    Created new Dataedo code test console application.

---
+++ ConsoleApp/DataReader.cs
@@ -0,0 +1,142 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class DataReader
+    {
+        IEnumerable<ImportedObject> ImportedObjects;
+
+        public void ImportAndPrintData(string fileToImport, bool printData = true)
+        {
+            // Error5: Not need to set empty object creation when list object is initizlied.
+            //ImportedObjects = new List<ImportedObject>() { new ImportedObject() };
+            ImportedObjects = new List<ImportedObject>() {};
+
+            var streamReader = new StreamReader(fileToImport);
+
+            var importedLines = new List<string>();
+            while (!streamReader.EndOfStream)
+            {
+                var line = streamReader.ReadLine();
+                importedLines.Add(line);
+            }
+
+            for (int i = 0; i < importedLines.Count; i++)
+            {
+                var importedLine = importedLines[i];
+                var values = importedLine.Split(';');
+                var importedObject = new ImportedObject();
+
+                // Error3: Prevent Null Reference Exception.
+                // If CSV first colum is empty we will skip that row.
+                if (!String.IsNullOrEmpty(values[0]))
+                {
+                    // Error4: To save array index out of range expection validate each property value exist.                    
+                    importedObject.Type = values.Length > 0 ? values[0] : string.Empty;
+                    importedObject.Name = values.Length > 1 ? values[1] : string.Empty;
+                    importedObject.Schema = values.Length > 2 ? values[2] : string.Empty;
+                    importedObject.ParentName = values.Length > 3 ? values[3] : string.Empty;
+                    importedObject.ParentType = values.Length > 4 ? values[4] : string.Empty;
+                    importedObject.DataType = values.Length > 5 ? values[5] : string.Empty;
+                    importedObject.IsNullable = values.Length > 6 ? values[6] : string.Empty;
+                    ((List<ImportedObject>)ImportedObjects).Add(importedObject);
+                }
+            }
+
+            // clear and correct imported data
+            foreach (var importedObject in ImportedObjects)
+            {
+                importedObject.Type = importedObject.Type.Trim().Replace(" ", "").Replace(Environment.NewLine, "").ToUpper();
+                importedObject.Name = importedObject.Name.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                importedObject.Schema = importedObject.Schema.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                importedObject.ParentName = importedObject.ParentName.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                importedObject.ParentType = importedObject.ParentType.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+            }
+
+            // assign number of children            
+            for (int i = 0; i < ImportedObjects.Count(); i++)
+            {
+                var importedObject = ImportedObjects.ToArray()[i];
+                foreach (var impObj in ImportedObjects)
+                {
+                    if (impObj.ParentType == importedObject.Type)
+                    {
+                        if (impObj.ParentName == importedObject.Name)
+                        {
+                            importedObject.NumberOfChildren = 1 + importedObject.NumberOfChildren;
+                        }
+                    }
+                }
+            }
+
+            foreach (var database in ImportedObjects)
+            {
+                if (database.Type == "DATABASE")
+                {
+                    Console.WriteLine($"Database '{database.Name}' ({database.NumberOfChildren} tables)");
+
+                    // print all database's tables
+                    foreach (var table in ImportedObjects)
+                    {
+                        if (table.ParentType.ToUpper() == database.Type)
+                        {
+                            if (table.ParentName == database.Name)
+                            {
+                                Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
+
+                                // print all table's columns
+                                foreach (var column in ImportedObjects)
+                                {
+                                    if (column.ParentType.ToUpper() == table.Type)
+                                    {
+                                        if (column.ParentName == table.Name)
+                                        {
+                                            Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class ImportedObject : ImportedObjectBaseClass
+    {
+        //Error 1 - Name property already defined in its parent class.
+        // So not repeast this again in Child class.
+        //public string Name
+        //{
+        //    get;
+        //    set;
+        //}
+
+        public string Schema;
+
+        public string ParentName;
+        public string ParentType
+        {
+            get; set;
+        }
+
+        public string DataType { get; set; }
+        public string IsNullable { get; set; }
+
+        public double NumberOfChildren;
+    }
+
+    class ImportedObjectBaseClass
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new DataReader();
+
+            //Error 2 - Referring CSV file name is not correct. 
+            reader.ImportAndPrintData("data.csv");
+        }
+    }
+}
